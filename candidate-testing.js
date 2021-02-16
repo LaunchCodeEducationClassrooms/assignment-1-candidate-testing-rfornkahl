5fa3c256aa2e5a467279cab6e4c5004e36714b58
rwfornkahl <rwfornkahl@gmail.com>
2021-02-16 01:41:16 +0000

    removed clear content function that was used to match the output that is displayed in assignment 1 instructions

--- candidate-testing.js
+++ candidate-testing.js
@@ -16,7 +16,6 @@ let candidateAnswers = [];
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 candidateName = input.question("Please provide your name: ");
-console.clear();
 }
 
 function askQuestion() {
